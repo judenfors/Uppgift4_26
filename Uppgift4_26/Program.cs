6e90aa9b553886ad1bb247c8443142350988aade
TheBlueSoul5188 <89245290+BaoLanNguyen@users.noreply.github.com>
2022-10-21 10:32:43 +0200

    Add project files.

---
+++ Uppgift4_26/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift4_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett heltal");
+            string tal1 = Console.ReadLine();
+
+            for(int i = 0; i < tal1.Length; i++)
+            {
+                string tecken1 = tal1[i].ToString();
+                int siffra1 = int.Parse(tecken1);
+
+                switch (siffra1)
+                {
+                    case 9:
+                        siffra1 = 0;
+                        break;
+                    default:
+                        siffra1++;
+                        break;
+
+
+                }
+                Console.Write(siffra1);
+            }
+        }
+    }
+}
